dab2a4514292a8ddb4aa4cda7b1fe0b4fb6e5db3
SPeats <srpeters44@gmail.com>
2021-05-02 19:43:57 -0700

    fixing cors issue with origin

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ input();
 
 function input (){
     var state = localStorage.getItem('item');
-    url = "http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+state+"&format=json";
+    url = "http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+state+"&format=json&origin=*";
     fetch(url)
         .then(function (response) {
             return response.json();
@@ -16,7 +16,7 @@ function input (){
             console.log(data);
           var title = data.query.search[0].title;
           var pageId = data.query.search[0].pageid;
-          fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles='+title+'&exintro=1')
+          fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles='+title+'&exintro=1&origin=*')
           .then(function (responseAgain) {
             return responseAgain.json();
           })
