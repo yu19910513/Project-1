85a3542b6a92972f7755eeb30dd671a948f82f25
SPeats <srpeters44@gmail.com>
2021-05-04 15:09:18 -0700

    working on the search history function

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,15 +1,36 @@
 // Event listeners for homepage (index.html).
   //flag buttons
+  var stateNameArr = [];
+
   $('.eventBtn').on("click", function(event) {
     var stateName = event.target.value;
-    console.log(event.target.text);
+    console.log(event.target.value);
     localStorage.setItem('stateVisited', stateName)
-    window.location.href = 'page2.html';
+    // window.location.href = 'page2.html';
+    stateNameArr.push(stateName);
+    localStorage.setItem('stateVisitedArr', JSON.stringify(stateNameArr));
+
   })
 
   //dropdown list
-  $('a').on("click", function(event) {
-    var stateName = event.target.text;
-    console.log(event.target.text);
-    localStorage.setItem('stateVisited', stateName)
-  });
+  // $('a').on("click", function(event) {
+  //   var stateName = event.target.value;
+  //   console.log(event.target.value);
+  //   localStorage.setItem('stateVisited', stateName)
+  //   stateNameArr.push(stateName);
+  //   localStorage.setItem('stateVisitedArr', JSONstringify(stateNameArr))
+    
+  // });
+
+  $('.displaySearchHistory').on("click", function(stateVisitedArr){
+    var travelLocations = JSON.parse(localStorage.getItem('stateVisitedArr'));
+    console.log(travelLocations);
+    for (var i=0; i < travelLocations.length; i++){
+       var travelListItem = $('li').text(travelLocations[i]);
+      $('#searchHistory').append(travelListItem);
+    } 
+  
+  })
+
+
+
